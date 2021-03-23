8c23c0fba876e0b8902fca74bc00f8bb839b51b9
monikarashmi <monika.enc2016@gmail.com>
2021-03-23 17:26:09 -0400

    Create AN_vaccine_distribution_01.py
    I am using the data from kaggle.com for analyzing vaccine distribution and progress across the world. Hope my skills are helpful in fighting this pendamic. Wish me luck!!

---
+++ AN_vaccine_distribution_01.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mlt
+df=pd.read_csv('country_vaccinations.csv')
+#print(df.columns)
+#print(df.head())
+#print(df.tail())
+print(df['vaccines'].unique())
+#df.drop_duplicates(subset ="vaccines", keep = False, inplace = True) 
+
+#print(df.sort_values("vaccines", inplace=True) )
+#print(df['country'].nunique())
+
+
+#df = df.groupby(['country', 'vaccines']).size()
+#print (df)
+#filter1=df["vaccines"]=='Sputnik V'
+#print(filter1)
+#print(df.where(filter1, inplace = True) )
+#print(df['country'])
+
+if df['vaccines'] == 'Pfizer/BioNTech'
+          print(df['country'])
+ #else: 
+     print('dkdkdk')
+     #print('country not found')
