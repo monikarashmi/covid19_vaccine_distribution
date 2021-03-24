67b3629d61252945164eb1f883785b6fd1b8877f
monikarashmi <monika.enc2016@gmail.com>
2021-03-24 18:41:10 -0400

    Country to vaccine mapping
    What vaccines are used and in which countries?
    Results stored under Result2.csv file

--- AN_vaccine_distribution_01.py
+++ AN_vaccine_distribution_01.py
@@ -4,23 +4,7 @@ import matplotlib.pyplot as mlt
 df=pd.read_csv('country_vaccinations.csv')
 #print(df.columns)
 #print(df.head())
-#print(df.tail())
-print(df['vaccines'].unique())
-df.drop_duplicates(subset ="vaccines", keep = False, inplace = True) 
-
-#print(df.sort_values("vaccines", inplace=True) )
-#print(df['country'].nunique())
+print(df.tail())
 
 
-#df = df.groupby(['country', 'vaccines']).size()
-#print (df)
-#filter1=df["vaccines"]=='Sputnik V'
-#print(filter1)
-#print(df.where(filter1, inplace = True) )
-#print(df['country'])
 
-if df['vaccines'] == 'Pfizer/BioNTech'
-          print(df['country'])
- #else: 
-     print('dkdkdk')
-     #print('country not found')
